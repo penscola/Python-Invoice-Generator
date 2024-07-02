de45e8cc1a7dae8596398f3f71ca3fe9b4be2412
penscola <penscolashackletonfelix@gmail.com>
2024-07-02 03:45:07 +0300

    Python GUI Automation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import tkinter
 from tkinter import ttk
+from docxtpl import DocxTemplate
+import datetime
+from tkinter import messagebox
 
 
 def clear_item():
@@ -10,15 +13,20 @@ def clear_item():
     unit_price_spinbox.insert(0, '0.0')
 
 
+invoice_list = []
+
+
 def add_item():
     qty = int(qty_spinbox.get())
     desc = description_entry.get()
     price = float(unit_price_spinbox.get())
-    line_total = qty*price
+    line_total = qty * price
     invoice_item = [qty, desc, price, line_total]
 
     tree.insert('', 0, values=invoice_item)
     clear_item()
+
+    invoice_list.append(invoice_item)
 
 
 def new_invoice():
@@ -27,6 +35,33 @@ def new_invoice():
     phone_entry.delete(0, tkinter.END)
     clear_item()
     tree.delete(*tree.get_children())
+
+    invoice_list.clear()
+
+
+def generate_invoice():
+    doc = DocxTemplate('invoice_template.docx')
+    name = first_name_entry.get() + ' ' + last_name_entry.get()
+    phone = phone_entry.get()
+    subtotal = sum(item[3] for item in invoice_list)
+    salestax = 0.1
+    total = subtotal*(1-salestax)
+
+    doc.render({
+        'name': name,
+        'phone': phone,
+        'invoice_list': invoice_list,
+        'subtotal': subtotal,
+        'salestax': str(salestax * 100) + '%',
+        'total': total
+    })
+
+    doc_name = 'new_invoice_' + name + datetime.datetime.now().strftime('%Y-%m-%d-%H%M%S') + '.docx'
+    doc.save(doc_name)
+
+    messagebox.showinfo('Invoice Generated Successfully')
+
+    new_invoice()
 
 
 window = tkinter.Tk()
@@ -80,7 +115,7 @@ tree.heading('total', text='Price')
 
 tree.grid(row=5, column=0, columnspan=3, padx=20, pady=10)
 
-save_invoice_button = tkinter.Button(frame, text='Generate Invoice')
+save_invoice_button = tkinter.Button(frame, text='Generate Invoice', command=generate_invoice)
 save_invoice_button.grid(row=6, column=0, columnspan=3, sticky='news', padx=20, pady=5)
 new_invoice_button = tkinter.Button(frame, text='New Invoice', command=new_invoice)
 new_invoice_button.grid(row=7, column=0, columnspan=3, sticky='news', padx=20, pady=5)
